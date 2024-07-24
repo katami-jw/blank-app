71a6751929b44c9844b3fbd2db0d4cf18e8da67d
miyuki katayama <165630128+katami-jw@users.noreply.github.com>
2024-07-24 12:36:11 +0000

    微修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,7 @@ import streamlit as st
 import pandas as pd
 
 data = {"menu" : ["pepperoni","teriyaki","margherita","thin crust","pan style crust","cheese","tomato","habanero","chicken"],"cost" : [1800,2100,1900,0,100,100,150,100,200]}
-#data = {"pepperoni":1800, "teriyaki":2100, "margherita":1900, "thin crust":0, "pan style crust":100, "cheese":100, "tomato":150, "pepperoni":100, "chicken":200}
 df_data = pd.DataFrame(data)
-
-#st.write(df_data)
 
 st.title('pizza order')
 
@@ -27,6 +24,4 @@ order = toppings + [pizza,crust]
 
 df_order = df_data.query('menu in @order')
 
-st.write(sum(df_order["cost"]))
-
-st.write('total amount')
+st.write('total amount　▶ ',sum(df_order["cost"]))
